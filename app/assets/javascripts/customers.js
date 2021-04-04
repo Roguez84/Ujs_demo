9bde7d60c6259b062fd9098ccd15726c1c51ff22
Roguez84 <t.roguez984@gmail.com>
2021-04-04 19:02:51 -0400

    changed code to do same thing with js and not jquery

--- app/assets/javascripts/customers.js
+++ app/assets/javascripts/customers.js
@@ -1,14 +1,15 @@
-document.addEventListener('turbolinks:load', function(){
-    var links = document.getElementsByTagName('a')
-    for(link of links){
-        link.addEventListener('click', function(e){
-            e.preventDefault()
-            $.ajax({
-                url: e.target.href,
-                method: 'get',
-                dataType: 'script',
-                error: function(error){console.log(error)}
-            })
-        })
-    }
-})
+//If we wanted to do this with jquery, use the code below. Otherwise, comment out and use remote:true in index view
+// document.addEventListener('turbolinks:load', function(){
+//     var links = document.getElementsByTagName('a')
+//     for(link of links){
+//         link.addEventListener('click', function(e){
+//             e.preventDefault()
+//             $.ajax({
+//                 url: e.target.href,
+//                 method: 'get',
+//                 dataType: 'script',
+//                 error: function(error){console.log(error)}
+//             })
+//         })
+//     }
+// })
